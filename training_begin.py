fa683d7503a3c1a1cfbd3ed7481482d994568e56
Konefal <rafal.konefal@nokia.com>
2020-03-01 00:09:36 +0100

    dwa punkty na raz

--- training_begin.py
+++ training_begin.py
@@ -1,22 +1,28 @@
 import cv2
 from paddles import detect_paddles
 from define_traces import define_trace
+from random import seed
+from random import randint
 def training_begin (cap, borders):
+    seed(5)
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)  # odbicie lustrzane
     points = define_trace(borders)
-    point = points.get()
+    point1 = points.get()
+    point2 = points.get()
     while (not points.empty()):
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)  # odbicie lustrzane
         bin = detect_paddles(frame)
-        if point_reched(point,frame):
-            point = points.get()
-        frame = cv2.circle(frame, (point[0], point[1]), 2, (255, 0, 0), 50)
+        if point_reched(point1,frame) and point_reched(point2,frame):
+            point1 = points.get()
+            point2 = points.get()
+        frame = cv2.circle(frame, (point1[0], point1[1]), 2, (255, 0, 0), 10)
+        frame = cv2.circle(frame, (point2[0], point2[1]), 2, (255, 0, 0), 10)
         cv2.imshow("img",frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    return 1
+    return randint(0,1000)
 
 def point_reched(point,frame):
     bin = detect_paddles(frame)
