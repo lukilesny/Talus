4e4b8b2c4c493db3f1cd68e25ca079003de8a2f0
Konefal <rafal.konefal@nokia.com>
2020-03-01 02:54:04 +0100

    sending request

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import time
 from threading import Timer
 from capture_video import capture_video
 from questionbox import questionBox
+from send_request import send_request
+import socket
 def main():
     starttime = time.time()
     while(True):
-        time.sleep(10.0 - ((time.time() - starttime) % 10.0))
+        time.sleep(2.0 - ((time.time() - starttime) % 2.0))
         if questionBox():
             capture_video()
-
+        else:
+            send_request(socket.gethostname(), 0)
 if __name__ == "__main__":
     main()
 
