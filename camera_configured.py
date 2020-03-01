6d14033fbec196a8eb1858bf188593acf17ce4e2
Michal <krzyszczuk.michal@gmail.com>
2020-03-01 01:47:39 +0100

    working version

--- camera_configured.py
+++ camera_configured.py
@@ -21,20 +21,23 @@ def camera_configured(cap):
                 height = len(rawframe)
                 width = len(rawframe[0])
                 scale_x = int(1.6 * face_y)
-                left_point_x = max(human_core_x - scale_x, 0)
-                left_point_y = 0
-                right_point_x = min(human_core_x + scale_x, width - 1)
-                right_point_y = height - 1
+                left_point_x = max(human_core_x - scale_x, 0)+5
+                left_point_y = 5
+                right_point_x = min(human_core_x + scale_x, width - 1)-5
+                right_point_y = height - 6
                 if debug:
                     rawframe = cv2.circle(rawframe, (face_x, face_y), 15, (255, 0, 0), 2)
                     rawframe = cv2.circle(rawframe, (human_core_x, human_core_y), 15, (255, 255, 0), 2)
                     rawframe = cv2.rectangle(rawframe, (left_point_x, left_point_y), (right_point_x, right_point_y),
                                              (255, 0, 0), 2)
-                    cv2.imshow("frame", rawframe)
+                    cv2.imshow("window", rawframe)
                     cv2.waitKey(0)
-                return [left_point_x, left_point_y, right_point_x, right_point_y]
+                cv2.destroyAllWindows()
+                return [int(round(left_point_x)),int(round( left_point_y)), int(round(right_point_x)), int(round(right_point_y))]
 
-        cv2.imshow("frame", rawframe)
+        cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        cv2.imshow("window", rawframe)
         cv2.imshow("binary", bin)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
